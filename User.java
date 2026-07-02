ddb03aff7791bf225e275fb176f43853addafa9f
praju12386 <zenderenuka658@gmail.com>
2026-07-02 22:01:34 +0530

    Add files via upload

---
+++ User.java
@@ -0,0 +1,18 @@
+public class User {
+    private final String name;
+    private final int userId;
+
+
+    public User(String name, int userId) {
+        this.name = name;
+        this.userId = userId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+}
